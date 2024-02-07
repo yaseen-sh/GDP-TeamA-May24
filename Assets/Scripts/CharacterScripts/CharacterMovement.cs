0d515e13de4166ffed21ace8f1f0bb3dc4e965d2
Rosa Kilmer <jkilmer3@kent.edu>
2024-02-06 20:01:57 -0500

    Basic Movement
    Character can move left and right and jump. Need to implement ground checks and crouching as well as primitive animations and begin to figure out 2 player control

--- Assets/Scripts/CharacterScripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterScripts/CharacterMovement.cs
@@ -1,39 +1,34 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour
 {
-    //This is just kind of put in from the tutorial video Dylan posted in general, we can change this later and should.
-    //This is just for basic functionality and should not be final. Please give feedback as this is something I care deeply about.
-    
-
-    private CharacterController controller;
-    private Vector2 playerVelocity;
-    public float speed = 2.0f;
-    public float jumpHeight = 1.0f;
-    public float gravity = -9.81f; //This should probably change
-    public Vector2 moveInput = Vector2.zero;
-    public bool jumping = false;
+    public Rigidbody2D rb;
+    private float horizontal; //Input float for 2D movement
+    public float speed;
+    private bool isGrounded = true;
+    public float jumpHeight;
 
     void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>(); 
-    }
-
-    public void onMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
-
-    public void onJump(InputAction.CallbackContext context)
-    {
-        jumping = context.ReadValue<bool>();
-        jumping = context.action.triggered;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    public void Movement(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump (InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
     }
 }
