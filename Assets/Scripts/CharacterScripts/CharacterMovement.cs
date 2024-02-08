ce3e384514bc67e13fec06c9e89b1f1665d39a53
Yaseen Shaikh <yshaikh@kent.edu>
2024-02-08 09:03:06 -0500

    Jumping and Crouching somewhat functional

--- Assets/Scripts/CharacterScripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterScripts/CharacterMovement.cs
@@ -7,7 +7,12 @@ public class CharacterMovement : MonoBehaviour
     private float horizontal; //Input float for 2D movement
     public float speed;
     private bool isGrounded = true;
-    public float jumpHeight;
+
+    public LayerMask groundLayer; //ground layer so we know if we're above ground
+
+    public Transform groundCheck; //for checking if we're grounded.
+    public float groundCheckRadius = 0.1f; //radius around groundcheck for testing 
+    public float jumpForce = 5f;
 
     void Start()
     {
@@ -22,13 +27,26 @@ public class CharacterMovement : MonoBehaviour
     public void Movement(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+
+        //for crouching, is the player holding down s?
+        if(context.ReadValue<Vector2>().y < 0){
+            Crouch();
+        }
     }
 
     public void Jump (InputAction.CallbackContext context)
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
         if (context.performed && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
+
+    public void Crouch(){
+        //Set animation trigger for crouch
+        //collider related changes
+        Debug.Log("Crouching");
     }
 }
