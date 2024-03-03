4c542dae90c81ffdba75f5ea7a35f8e7937b447e
Dylan Sarell <116117025+dsarell1@users.noreply.github.com>
2024-03-02 19:57:20 -0500

    2 Player Cursors work. (the Basics). I need to work on keeping the cursors active between screens though.
    Also made it so that when both players select their characters, ready for battle text pops up. I also made it so that only when 2 controllers are plugged in can you go to the character select screen for PvP.

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -8,6 +8,15 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    private bool p1Select;
+    private bool p2Select;
+
+    public void Start()
+    {
+        p1Select = false;
+        p2Select = false;
+    }
+
     public void TitleScreen()
     {
         SceneManager.LoadScene("TitleScreen");
@@ -30,26 +39,119 @@ public class ButtonManager : MonoBehaviour
     }
 
     // ------------------------------------------------------
-    // Character Select Buttons
-
+    // Character Select Buttons - Probably Don't Need
+    /*
     public void SelectMeldin()
     {
+        Debug.Log("Meldin & Medo");
+        Sprite meldin = GameObject.FindGameObjectWithTag("CharGrid").GetComponent<SetupCharTiles>().fighters[0].charImage;
+        /*GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player1Fighter = meldin;
 
-    }
-    public void SelectGreg()
-    {
+        if (RectTransformUtility.RectangleContainsScreenPoint(meldin.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP1").transform.position))
+        {
+            //if (p1Select) p1Select = false;
+            //else p1Select = true;
 
-    }
-    public void SelectMikhail()
-    {
+            //Debug.Log(p1Select);
+            //GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player1Selected = p1Select;
+            //GameObject.FindGameObjectWithTag("CursorP1").GetComponent<GamepadCursor>().charSelected = 
+            return;
+        }
+        if (RectTransformUtility.RectangleContainsScreenPoint(meldin.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP2").transform.position))
+        {
+            //if (p2Select) p2Select = false;
+            //else p2Select = true;
 
+            //GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player2Selected = p2Select;
+            return;
+        }
     }
     public void SelectBranson()
     {
+        Debug.Log("Branson");
+        GameObject branson = GameObject.FindGameObjectWithTag("CharGrid").GetComponent<SetupCharTiles>().allTiles[1];
 
+        if (RectTransformUtility.RectangleContainsScreenPoint(branson.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP1").transform.position))
+        {
+            if (p1Select) p1Select = false;
+            else p1Select = true;
+
+            GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player1Selected = p1Select;
+            return;
+        }
+        if (RectTransformUtility.RectangleContainsScreenPoint(branson.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP2").transform.position))
+        {
+            if (p2Select) p2Select = false;
+            else p2Select = true;
+
+            GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player2Selected = p2Select;
+            return;
+        }
+    }
+    public void SelectGreg()
+    {
+        Debug.Log("GREG");
+        GameObject greg = GameObject.FindGameObjectWithTag("CharGrid").GetComponent<SetupCharTiles>().allTiles[2];
+
+        if (RectTransformUtility.RectangleContainsScreenPoint(greg.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP1").transform.position))
+        {
+            if (p1Select) p1Select = false;
+            else p1Select = true;
+
+            GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player1Selected = p1Select;
+            return;
+        }
+        if (RectTransformUtility.RectangleContainsScreenPoint(greg.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP2").transform.position))
+        {
+            if (p2Select) p2Select = false;
+            else p2Select = true;
+
+            GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player2Selected = p2Select;
+            return;
+        }
+    }
+    public void SelectMikhail()
+    {
+        Debug.Log("NESTY");
+        GameObject nesty = GameObject.FindGameObjectWithTag("CharGrid").GetComponent<SetupCharTiles>().allTiles[3];
+
+        if (RectTransformUtility.RectangleContainsScreenPoint(nesty.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP1").transform.position))
+        {
+            if (p1Select) p1Select = false;
+            else p1Select = true;
+
+            GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player1Selected = p1Select;
+            return;
+        }
+        if (RectTransformUtility.RectangleContainsScreenPoint(nesty.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP2").transform.position))
+        {
+            if (p2Select) p2Select = false;
+            else p2Select = true;
+
+            GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player2Selected = p2Select;
+            return;
+        }
     }
     public void SelectBill()
     {
+        Debug.Log("Bill");
+        GameObject bill = GameObject.FindGameObjectWithTag("CharGrid").GetComponent<SetupCharTiles>().allTiles[4];
 
-    }
+        if (RectTransformUtility.RectangleContainsScreenPoint(bill.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP1").transform.position))
+        {
+            if (p1Select) p1Select = false;
+            else p1Select = true;
+
+            GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player1Selected = p1Select;
+            return;
+        }
+        if (RectTransformUtility.RectangleContainsScreenPoint(bill.GetComponent<RectTransform>(), GameObject.FindGameObjectWithTag("CursorP2").transform.position))
+        {
+            if (p2Select) p2Select = false;
+            else p2Select = true;
+
+            GameObject.FindGameObjectWithTag("CharManager").GetComponent<CharManager>().player2Selected = p2Select;
+            return;
+        }
+    }*/
 }
