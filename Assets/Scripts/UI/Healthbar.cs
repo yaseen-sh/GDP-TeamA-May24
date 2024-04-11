cdc5ea466a461156955574b5648ed104094c4f10
Chris <icentrick@gmail.com>
2024-04-10 20:11:50 -0400

    healthbar!

--- Assets/Scripts/UI/Healthbar.cs
+++ Assets/Scripts/UI/Healthbar.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
     public static int direction = 1;
-    public Image filledBar;
-    public int filledPercent = 100;
-    public int newPercent = 100;
+    private Image filledBar;
+    private Slider sliderBar;
+    public GameObject filledBarGO;
+    public double filledPercent = 100;
+    public double newPercent = 100;
 
     // Dealt with in percents so the actual health isnt relevant to how the bar acts
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sliderBar = filledBarGO.GetComponent<Slider>();
+        //filledBar.type = Image.Type.Filled;
     }
     
     // Update is called once per frame
@@ -22,13 +26,15 @@ public class Healthbar : MonoBehaviour
     {
         if(filledPercent > newPercent)
         {
-            filledPercent--;
+            filledPercent-=0.5;
+            sliderBar.value = ((float)(filledPercent / 100));
+          //  Debug.Log((float)(filledPercent/100));
         }
     }
 
-    void SubtractHealth(int current, int DecreaseAmount)
+    void SubtractHealth(int initialHealth, int DecreaseAmount)
     {
-        newPercent = filledPercent - (current - DecreaseAmount)/100;
+        newPercent = filledPercent - DecreaseAmount/initialHealth;
     }
 
     void resetHealth()
