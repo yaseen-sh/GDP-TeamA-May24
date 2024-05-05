b8a86ea97ad532fdd314c88c2f7cfa067d9d5fb9
Yaseen Shaikh <yshaikh@kent.edu>
2024-05-05 16:56:22 -0400

    Working on nesty super

--- Assets/Scripts/NestySuper1.cs
+++ Assets/Scripts/NestySuper1.cs
@@ -7,30 +7,14 @@ public class NestySuper1 : MonoBehaviour
 
     public GameObject superball;
     public GameObject shardsPrefab;
-    public int numberOfShards = 5;
-    public GameObject opponent;
-    Hitbox hitbox;
-
-    private CharacterManager OpponentTag;
-    ParticleSystem particle;
+    public int numberOfShards = 7;
     Vector2 pos;
-
-    private void Awake()
-    {
-        hitbox = GetComponent<Hitbox>();
-        OpponentTag = GameObject.FindGameObjectWithTag("Player 2").GetComponent<CharacterManager>();
-        
-    }
 
     // Start is called before the first frame update
     void Start()
     {
-        particle = GetComponent<ParticleSystem>();
-        opponent = GameObject.FindGameObjectWithTag("Player 2");
-
         //want to find the object
         superball = GameObject.FindGameObjectWithTag("superBall");
-
         pos = superball.transform.position;
     }
 
@@ -38,19 +22,22 @@ public class NestySuper1 : MonoBehaviour
     void Update()
     {
         // until the ball reaches the desired position
-        if(superball.transform.position.x != 0 || superball.transform.position.y != 3)
+        if (superball.transform.position.x != 0 || superball.transform.position.y != 3)
         {
             //move it to the middle at a certain speed
             superball.transform.position = Vector3.MoveTowards(pos, new Vector2(0, 3), 5f * Time.deltaTime);
         }
+
         //now we've reached the point
-        else{
+        else
+        {
             //play explosive particle system
             //shoot out shards at slightly varying angles
-            for(int i = 0; i < numberOfShards; ++i){
+            for (int i = 0; i < numberOfShards; ++i)
+            {
                 StartCoroutine(wait(1)); //space out the timing of shards by a bit
                 GameObject s = Instantiate(shardsPrefab, superball.transform);//instantiate prefab
-                s.transform.GetChild(0).Rotate(0,0, Random.Range(-30f, 30f)); //slightly randomize the location
+                s.transform.GetChild(0).Rotate(0, 0, Random.Range(-30f, 30f)); //slightly randomize the location
                 s.GetComponent<Rigidbody2D>().AddForce(s.transform.up * -10f); //launch it 
             }
             Destroy(gameObject);
@@ -58,27 +45,6 @@ public class NestySuper1 : MonoBehaviour
             //will explode on collision
         }
 
-        
-    }
-
-    void OnTriggerEnter2D(Collider2D coll){
-        //Debug.Log("ontrigger");
-        if (coll.gameObject.CompareTag("HurtBox") && hitbox.isAttacking == true)
-        {
-            //Debug.Log(coll.gameObject.name);
-            OpponentTag.GetPlayerHealth();
-            OpponentTag.SetPlayerHealth(2,1);
-            //Debug.Log("Hit Confirmed");
-        }
-        else if (coll.gameObject.CompareTag("BlockBox"))
-        {
-            OpponentTag.GetPlayerHealth();
-            OpponentTag.SetPlayerHealth(1, 1);
-        }
-
-        //explode and destroy here
-        //getComponentInChildren<SpriteRenderer>().sprite = "Assets/Sprites/NesterenkoSprites/nesty_super_5";
-        Destroy(gameObject);
     }
     
     IEnumerator wait (float f)
